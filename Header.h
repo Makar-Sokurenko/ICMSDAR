aa5603536ee5eca5e0f44ba38b4d9af3fc87697c
Makar-Sokurenko <makarsokurenko2006@gmail.com>
2024-12-20 10:40:38 +0300

    Update Header.h

--- Header.h
+++ Header.h
@@ -14,7 +14,6 @@ public:
 void copyProp(xlnt::workbook source_wb, xlnt::workbook new_wb);
 void create_Sample(string subj_names[100], int subj_num, int grades_num[20]);
 void load_xlsx(xlnt::workbook wb, string subj_names[100], int subj_num, int grades_num[20], Student students[100], int numStudents, double grades[100][100][100]);
-//void save_xlsx(xlnt::workbook wb, string subj_names[100], int subj_num, int grades_num[20], Student students[100], int numStudents, double grades[100][100][100]);
 void save_xlsx(xlnt::workbook wb, xlnt::workbook new_wb, string subj_names[100], int subj_num, int grades_num[20], Student students[100], int numStudents, double grades[100][100][100]);
 void displayMenu();
 void enterStudentData(Student students[100], int n);
